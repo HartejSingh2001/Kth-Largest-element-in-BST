1866938992746a40c23ef4d19eafd06f991a9595
HartejSingh2001 <86732914+HartejSingh2001@users.noreply.github.com>
2022-02-12 14:50:13 +0530

    Create large.cpp

---
+++ large.cpp
@@ -0,0 +1,25 @@
+// return the Kth largest element in the given BST rooted at 'root'
+class Solution
+{
+    public:
+    int kthLargest(Node *root, int K)
+    {
+        if(root==NULL)
+        {
+            return {};
+        }
+        vector<int>v;
+        inorder(root,K,v);
+        int size=v.size();
+        return v.at(size-K);
+    }
+    void inorder(Node*root,int K, vector<int>&v)
+    {
+        if(root!=NULL)
+        {
+            inorder(root->left,K,v);
+            v.push_back(root->data);
+            inorder(root->right,K,v);
+        }
+    }
+};
